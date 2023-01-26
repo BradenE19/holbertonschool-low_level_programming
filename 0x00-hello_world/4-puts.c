16569cdad587aaf0ee44fe8b7a7e301204af57c5
Braden <blearnest1995@gmail.com>
2023-01-26 09:37:29 -0800

    0x00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
-#!/bin/bash
-gcc -Wall
 #include <stdio.h>
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
-	Return (0);
+	puts("\"Programming is like building a multilingual puzzle")
+	Return(0);
 }
