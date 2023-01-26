adfe39f4f8a020105c908f5b5c63cd9172fc6113
Braden <blearnest1995@gmail.com>
2023-01-26 12:53:31 -0800

    printalphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char abc;
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+if ((abc != 'q') && (abc != 'e'))
+putchar (abc);
+}
+putchar ('\n');
+return (0);
+}
