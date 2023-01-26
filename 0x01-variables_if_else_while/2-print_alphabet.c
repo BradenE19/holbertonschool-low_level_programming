706172ee4dc0dd62c293b730e070a17f202d7dc7
Braden <blearnest1995@gmail.com>
2023-01-26 12:16:13 -0800

    last digit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,11 +3,14 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char abc;
+
 	for (abc = 'a'; abc <= 'z'; abc++)
 	putchar (abc);
 	putchar ('\n');
 	return (0);
+
 }
