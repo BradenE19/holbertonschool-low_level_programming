1bff95a84227b643489d3578b9da9b737a184c12
Braden <blearnest1995@gmail.com>
2023-01-27 11:03:13 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char abc;
+int num;
+for (num = 0; num <= 9; num++)
+putchar (num + '0');
+for (abc = 'a'; abc <= 'f'; abc++)
+putchar (abc);
+putchar ('\n');
+return (0);
+}
