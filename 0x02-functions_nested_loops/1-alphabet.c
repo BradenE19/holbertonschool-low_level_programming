fc7fc5789e1ed461c5f06e085866f388cffaadcf
Braden <blearnest1995@gmail.com>
2023-01-30 10:27:52 -0800

    0x02

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main _ Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
