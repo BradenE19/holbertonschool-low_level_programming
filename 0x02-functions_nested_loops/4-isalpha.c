8622af36229f137c024dd8fff75b9d719d6bca21
Braden <blearnest1995@gmail.com>
2023-01-30 13:02:25 -0800

    0x02

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - check the code for Holberton School students.
+ * @c: is a int variable
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+char i, j;
+for (i = 'a'; i < 'z'; i++)
+{
+if (c == i)
+return (1);
+}
+for (j = 'A'; j < 'Z'; j++)
+{
+if (c == j)
+return (1);
+}
+return (0);
+}
