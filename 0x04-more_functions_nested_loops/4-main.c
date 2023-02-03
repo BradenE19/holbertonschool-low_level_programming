442b083bbea06200438d7d5df1983b230ec669c2
Braden <blearnest1995@gmail.com>
2023-02-03 09:29:37 -0800

    0x04

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * print_most_numebers - function
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_most_numbers();
+    return (0);
+}
