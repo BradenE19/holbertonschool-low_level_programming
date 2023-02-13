19673fee674e4e8b3aa2f9f9050483ff9950568e
Braden <blearnest1995@gmail.com>
2023-02-13 13:02:23 -0800

    0x07

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 {
 	int i, n, total1 = 0, total2 = 0;
 
-	for (i = 0; i <= (size * size); i = 1 + size + 1)
+	for (i = 0; i <= (size * size); i = i + size + 1)
 		total1 = total1 + a[i];
 
 	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
