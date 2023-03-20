18c81da97693fb07b0d4c39acb9e1c0ee48cc78e
Braden <blearnest1995@gmail.com>
2023-03-20 12:58:13 -0700

    0x12

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - function that adds new node at beginning of list_t
+ * @head: pointer to pointer to list_t list
+ * @str: string to copy a new node
+ * Return: address of new element, or NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int i;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		new->str == stdrup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = i;
+	new->next = *head;
+	*head = new;
+	return(*head);
+}
