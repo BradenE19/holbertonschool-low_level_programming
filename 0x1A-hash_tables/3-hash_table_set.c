d2cbe42b0b5ae4a1f3b66aac992692692d7171e7
Braden <blearnest1995@gmail.com>
2023-03-27 11:29:01 -0700

    0x1a

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,42 +1,36 @@
 #include "hash_tables.h"
-
 /**
- *hash_table_set - function
- *@ht: variable
- *@key: variable
- *@value: variable
- *Return: value
+ *hash_table_set - adds an element to the hash table
+ *@ht: pointer to the hash
+ *@key: pointer to the key
+ *@value: value to add
+ *Return: 1 if is success or 0
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index, size;
-	hash_node_t *newNode;
+unsigned long index, size;
+hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || value == NULL)
-	{
-		return (0);
-	}
+if (ht  == NULL || key == NULL || value == NULL)
+return (0);
 
-	size = ht->size;
-	index = key_index((const unsigned char *)key, size);
-	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
-	{
-		ht->array[index]->value = strdup(value);
-		return (1);
-	}
-	else
-	{
-		newNode = (hash_node_t *)malloc(sizeof(hash_node_t));
+size = ht->size;
+index = key_index((const unsigned char *)key, size);
 
-		if (newNode == NULL)
-		{
-			return (0);
-		}
+if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+{
+ht->array[index]->value = strdup(value);
+return (1);
+}
 
-		newNode->key = strdup(key);
-		newNode->value = strdup(value);
-		newNode->next = ht->array[index];
-		ht->array[index] = newNode;
-		return (1);
-	}
+new_node = malloc(sizeof(hash_node_t));
+
+if (new_node == NULL)
+return (0);
+
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
+return (1);
 }
